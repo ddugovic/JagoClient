f97423e4fbb2d541e5e988686137d9f3b459108a
Daniel Dugovic <dandydand@gmail.com>
2016-07-18 08:03:33 -0500

    Add exception logging.

--- src/jagoclient/datagram/DatagramMessage.java
+++ src/jagoclient/datagram/DatagramMessage.java
@@ -1,7 +1,11 @@
 package jagoclient.datagram;
 
-import java.net.*;
-import java.util.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
 This class may be used to send a datagram to some internet
@@ -28,7 +32,10 @@ public class DatagramMessage
 			Used+=s.length();
 			B[Used]=0; Used++;
 		}
-		catch (Exception e) {}
+		catch (Exception ex)
+		{
+			Logger.getLogger(DatagramMessage.class.getName()).log(Level.WARNING, null, ex);
+		}
 	}
 	/**
 	Send the datagram to the specified address and port.
@@ -42,13 +49,16 @@ public class DatagramMessage
 			ds.send(dp);
 			ds.close();
 		}
-		catch (Exception e) {}
+		catch (Exception ex)
+		{
+			Logger.getLogger(DatagramMessage.class.getName()).log(Level.WARNING, null, ex);
+		}
 	}
 	/**
 	@return a vector of lines, containing the received datagram.
 	*/
-	public Vector receive (int port)
-	{	Vector v=new Vector();
+	public Vector<String> receive (int port)
+	{	Vector<String> v=new Vector<String>();
 		try
 		{	DatagramPacket dp=new DatagramPacket(B,MAX);
 			DatagramSocket ds=new DatagramSocket(port);
@@ -57,12 +67,15 @@ public class DatagramMessage
 			while (i<l)
 			{	int j=i; while (B[i]!=0) i++;
 				if (i>j) v.addElement(new String(B,j,i-j));
-				else v.addElement(new String(""));
+				else v.addElement("");
 				i++;
 			}
 			ds.close();
 		}
-		catch (Exception e) { return v; }
+		catch (Exception ex)
+		{
+			Logger.getLogger(DatagramMessage.class.getName()).log(Level.WARNING, null, ex);
+		}
 		return v;
 	}
 }
